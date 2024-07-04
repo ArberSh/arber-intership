748f231b29555d2471af57132fc6bb02b3bb87d5
Arber Shaska <arbershaska2@gmail.com>
2024-07-04 23:05:39 +0200

    Added Lazy Loading And Fetched Data in NewItems

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -3,22 +3,107 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const NewItems = () => {
 
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   const [GetData,SetGetData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [time,settime] = useState(0)
 
   useEffect(()=>{
     async function NewItemsData(){
     const {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
-    SetGetData(data)
+    SetGetData(data) 
+    setLoading(false)
   }
   NewItemsData()
+  Time()
   },[])
+
+  function Time(){
+    GetData.map(elem => {
+      const time = Date.now(elem.expiryDate)
+      console.log(time /1000 /60 /24 + "hi")
+    })
+}
+
+   
   
+  if(loading){
+    return(
+         <div className="container">
+         <div className="row">
+         <div className="col-lg-12">
+         <div className="text-center">
+              <h2>Hot Collections</h2>
+              <div className="small-border bg-color-2"></div>
+            </div>
+          </div>
+                    <div className=" Container-gray" style={{display:'flex'}}>
+                      <div className="gray"></div>
+                      <div className="gray"></div>
+                      <div className="gray"></div>
+                      <div className="gray"></div>
+                    </div>
+          </div>
+      </div>
+    )
+  }
 
   return (
+
     <section id="section-items" className="no-bottom">
+     
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -27,6 +112,7 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+          <Slider {...settings}>
           {GetData.map((elem, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
@@ -41,8 +127,6 @@ const NewItems = () => {
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
                     <div className="nft__item_buttons">
@@ -83,6 +167,7 @@ const NewItems = () => {
               </div>
             </div>
           ))}
+    </Slider>
         </div>
       </div>
     </section>
