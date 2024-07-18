2ed697e1a4937c1eb18a751e75eef636433ee486
Arber Shaska <arbershaska2@gmail.com>
2024-07-18 13:02:03 +0200

    Random message

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -8,6 +8,7 @@ import axios from "axios";
 const ItemDetails = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
+    //Any Comment
   }, []);
 
   const [loading,setLoading] = useState(true)
